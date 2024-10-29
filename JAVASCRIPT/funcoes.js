2b215ad7f545d2ef3abc55f4dfb9dea31add7855
maruhsilva <marua.desenvolvedor@gmail.com>
2024-10-28 21:31:18 -0300

    Commit Changes

--- JAVASCRIPT/funcoes.js
+++ JAVASCRIPT/funcoes.js
@@ -104,3 +104,13 @@ $('.cards1').slick({
       }
     ]
   });
+
+  function process(quant){
+    var value = parseInt(document.getElementById("quant").value);
+    value+=quant;
+    if(value < 1){
+      document.getElementById("quant").value = 1;
+    }else{
+    document.getElementById("quant").value = value;
+    }
+  }
